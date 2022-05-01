09a88787d980c92e70a3a036532c30990b8d1994
Mulyono Putra <mulyonoputra27@outlook.com>
2022-05-01 16:40:45 +0700

    Update PromiseComponent.js

--- src/async-await-promise/components/PromiseComponent.js
+++ src/async-await-promise/components/PromiseComponent.js
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const PromiseComponent = () => {
+  const [text, setText] = useState("");
+
+  const onSubmit = () => {
+    alert(text);
+  };
+
   return (
     <div>
       {" "}
@@ -6,12 +14,16 @@ const PromiseComponent = () => {
         <div className="container mt-4">
           <div className="card">
             <div className="card-body">
-              <button type="button" className="btn btn-primary">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => {
+                  onSubmit();
+                }}
+              >
                 Primary
               </button>
-              <div className="card-body">
-                This is some text within a card body.
-              </div>
+              <div className="card-body">{text}</div>
             </div>
           </div>
         </div>
