7c64d148df8ffc10a1bd3167fe6f37f95d700c03
Mulyono Putra <mulyonoputra27@outlook.com>
2022-05-01 16:40:38 +0700

    Example Callback Javascript

--- src/async-await-promise/components/CallbackComponent.js
+++ src/async-await-promise/components/CallbackComponent.js
@@ -1,4 +1,40 @@
+import { useState } from "react";
+
+const profiles = {
+  firstName: "Maryam",
+  lastName: "Jameela",
+  age: 2,
+};
+
 const CallbackComponent = () => {
+  const [text, setText] = useState("");
+  const [profile, setProfile] = useState(profiles);
+
+  const { firstName, lastName } = profile;
+
+  const callFirstName = (callback) => {
+    setTimeout(() => {
+      callback(firstName);
+    }, 2000);
+  };
+
+  const callLastName = (callback) => {
+    setTimeout(() => {
+      callback(lastName);
+    }, 2000);
+  };
+
+  const onSubmit = () => {
+    callFirstName((result) => {
+      callLastName((results) => {
+        const first = result;
+        const last = results;
+        const fullname = first + " " + last;
+        setText(fullname);
+      });
+    });
+  };
+
   return (
     <div>
       {" "}
@@ -6,12 +42,16 @@ const CallbackComponent = () => {
         <div className="container mt-4">
           <div className="card">
             <div className="card-body">
-              <button type="button" className="btn btn-primary">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => {
+                  onSubmit();
+                }}
+              >
                 Primary
               </button>
-              <div className="card-body">
-                This is some text within a card body.
-              </div>
+              <div className="card-body">{text}</div>
             </div>
           </div>
         </div>
